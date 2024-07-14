ec9b5866d235515f315d3254bbccee287598e734
Tamnemng <nguyndantdm6@gmail.com>
2024-07-14 17:28:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Input, Button, Radio, Space } from 'antd';
 
 function App() {
+  const [value, setValue] = useState(1);
+  const onChange = (e) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Web">
+      <header className="Web-header">
+        <div className='Logging'>
+          <Input placeholder='input username' className='input-field' />
+          <Input.Password placeholder="input password" className='input-field' />
+          <Button type="primary" className='input-field'>Logging</Button>
+          <Radio.Group onChange={onChange} value={value}>
+            <Space direction="vertical">
+              <Radio value={1} className='radio-button'>Sinh Viên</Radio>
+              <Radio value={2} className='radio-button'>Cán Bộ</Radio>
+            </Space>
+          </Radio.Group>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
