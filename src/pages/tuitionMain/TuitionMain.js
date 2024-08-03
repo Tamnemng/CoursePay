fc453767e551194e99777c5e8057415198d1c759
TuanAnh <lngtuananh09@gmail.com>
2024-08-03 20:39:28 +0700

    student list fix

--- src/pages/tuitionMain/TuitionMain.js
+++ src/pages/tuitionMain/TuitionMain.js
@@ -1,146 +1,15 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import './TuitionMain.css';
 import Header from "../../components/tuitionHeader";
 import Typography from "antd/es/typography/Typography";
-import { Select, Table, Input, Button, Space } from "antd";
-import { SearchOutlined } from '@ant-design/icons';
-import Highlighter from "react-highlight-words";
+import { Select, Input } from "antd";
+import StudentTable from "./StudentTable";
+
 const {Title, Text} = Typography;
 
 
-
 export default function TuitionMain() {
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef(null);
-  const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-  const handleReset = (clearFilters) => {
-    clearFilters();
-    setSearchText('');
-  };
-  const getColumnSearchProps = (dataIndex) => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-      <div
-        style={{
-          padding: 8,
-        }}
-        onKeyDown={(e) => e.stopPropagation()}
-      >
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{
-            marginBottom: 8,
-            display: 'block',
-          }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{
-              width: 90,
-            }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{
-              width: 90,
-            }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({
-                closeDropdown: false,
-              });
-              setSearchText(selectedKeys[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered) => (
-      <SearchOutlined
-        style={{
-          color: filtered ? '#1677ff' : undefined,
-        }}
-      />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{
-            backgroundColor: '#ffc069',
-            padding: 0,
-          }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      ),
-  });
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      width: '30%',
-      ...getColumnSearchProps('name'),
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-      width: '20%',
-      ...getColumnSearchProps('age'),
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-      ...getColumnSearchProps('address'),
-      sorter: (a, b) => a.address.length - b.address.length,
-      sortDirections: ['descend', 'ascend'],
-    },
-  ];
-
+  
   return (
     <div className="tuitionMain-container">
       <Header />
@@ -171,7 +40,7 @@ export default function TuitionMain() {
                 ]}
               />
             </div>
-            <Table columns={columns}/>
+            <StudentTable/>
           </div>
           <div className="payment">
             <Title level={5}>Danh sách hóa đơn</Title>
@@ -218,8 +87,20 @@ export default function TuitionMain() {
                 ]}
               />
             </div>
-            <Text>Mã sinh viên: </Text>
-            <Text>Họ tên sinh viên: </Text>
+            <div className="ops">
+              <Text>Mã sinh viên: </Text>
+              <Input
+                className="customInp"
+                readOnly
+              />
+            </div>
+            <div className="ops">
+              <Text>Họ tên sinh viên: </Text>
+              <Input
+                className="customInp"
+                readOnly
+              />
+            </div>
           </div>
         </div>
       </div>
